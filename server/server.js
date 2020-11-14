e1dd38acc297c51c21dd3b9fc83ac055b85f9c76
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-14 12:29:05 -0600

    Apollo server up and running

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const path = require('path');
+// const routes = require('./routes');
+
+//Import Apollo server
+const { ApolloServer } = require('apollo-server-express');
+
+//import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+//create new Apollo server and pass in schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-app.use(express.urlencoded({ extended: true }));
+//integrate Apollos server with Express application as middleware
+server.applyMiddleware({app});
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -14,8 +29,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
