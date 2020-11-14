d26778564aba26d497ad4cf44b687953509ecbfc
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-14 13:08:45 -0600

    typedefs created

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,47 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql `
-type Query {
-    helloWorld: String
-  }
+    type User {
+        _id: ID!
+        username: String!
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book{
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type: Auth{
+        token: String
+        user: [User]
+    }
+
+    type: savedBooks{
+        authors: [String]
+        description: String
+        title: String
+        bookId: String
+        image: String
+        link: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation{
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(savedBooks): User
+        removeBook(bookId: ID!): User
+    }
 `;
 
 module.exports = typeDefs;
