770a91e304e555a16ce67a152c19e2f50bd362eb
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-14 17:20:02 -0600

    apollo client and apollo provider installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+
+  uri: 'http://localhost:3001/graphql'
+})
+
+
 function App() {
   return (
+    <ApolloProvider client={client}>
+      
+    </ApolloProvider>
     <Router>
       <>
         <Navbar />
