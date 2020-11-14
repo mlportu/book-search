188a9d2d7e52f4c0a112c4e3e382bd269b49e698
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-14 14:20:10 -0600

    add user and login works without auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,12 +18,12 @@ const resolvers = {
       },
     },
     Mutation: { 
-        // addUser: async (parent, args) => {
-        //     const user = await User.create(args);
-        //     const token = signToken(user);
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            // const token = signToken(user);
 
-        //     return{ token, user }
-        // },
+            return user; 
+        },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({email});
 
@@ -35,8 +35,8 @@ const resolvers = {
             if(!correctPw){
                 throw new AuthenticationError('Incorrect Credentials');
             }
-            const token = signToken(user);
-            return { token, user };
+            // const token = signToken(user);
+            return user;
         },
     },
 
