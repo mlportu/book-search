e61cd1ebbf83a25e27db6b7910fdf7b9381fcc7d
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-14 16:46:41 -0600

    removeBook created

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -48,6 +48,17 @@ const resolvers = {
             }
 
             throw new AuthenticationError('You need to be logged in');
+        },
+        removeBook: async( parent, {bookId}, context) => {
+            if(context.user){
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id },
+                    { $pull: {savedBooks: bookId } },
+                    {new: true}
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in');
         }
     },
 
