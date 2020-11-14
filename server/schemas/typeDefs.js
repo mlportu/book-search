a37cbee0a87f378dbc979a78af68455484feb5d6
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-14 17:07:15 -0600

    server complete

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,7 +11,7 @@ const typeDefs = gql `
 
     type Book{
         bookId: ID!
-        authors: [String]
+        authors: String
         description: String!
         title: String!
         image: String
@@ -30,7 +30,7 @@ const typeDefs = gql `
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth 
-        saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String, link: String): User
+        saveBook(bookId: ID!    , authors: String, description: String!, title: String!, image: String, link: String): User
         removeBook(bookId: ID): User
     }
 `;
