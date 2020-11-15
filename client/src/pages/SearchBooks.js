b1d5cd17ba8048b63ff77709329b6c16c224c646
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-15 12:05:12 -0600

    click event working and id pulling

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -57,28 +57,29 @@ useEffect(() => {
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    // const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // // get token
+    // const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const { data } = await saveBook({
-        variables: { bookData: { ...bookToSave } },
-      });
-      console.log(savedBookIds);
+    // if (!token) {
+    //   return false;
+    // }
+    console.log("click");
+    console.log(bookId);
+    // try {
+    //   const { data } = await saveBook({
+    //     variables: { bookData: { ...bookToSave } },
+    //   });
+    //   console.log(savedBookIds    );
       
       
 
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
-    }
+    //   // if book successfully saves to user's account, save book id to state
+    //   setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    // } catch (err) {
+    //   console.error(err);
+    // }
   };
 
   return (
