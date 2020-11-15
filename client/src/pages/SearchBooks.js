450a485dfd2e3de5113f993a17fdf767117f0bc0
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-15 12:08:24 -0600

    bookToSave functioning, token validating

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -57,16 +57,17 @@ useEffect(() => {
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
-    // const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // // get token
-    // const token = Auth.loggedIn() ? Auth.getToken() : null;
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    // if (!token) {
-    //   return false;
-    // }
+    if (!token) {
+      return false;
+    }
     console.log("click");
-    console.log(bookId);
+    console.log(bookToSave);
+    console.log(token);
     // try {
     //   const { data } = await saveBook({
     //     variables: { bookData: { ...bookToSave } },
