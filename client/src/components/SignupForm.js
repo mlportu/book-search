f64ae1535e3e1cd45bceb69985b51debc03a600e
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-15 16:02:24 -0600

    signup form test

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -8,9 +8,14 @@ import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({
+     username: '', 
+     email: '', 
+     password: '' 
+    });
   // set state for form validation
   const [validated] = useState(false);
+
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
@@ -25,35 +30,42 @@ const SignupForm = () => {
   }, [error]);
 
   const handleInputChange = (event) => {
+
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+
+    setUserFormData({ 
+      ...userFormData, 
+      [name]: value 
+    });
   };
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log("signup clicked");
+    console.log(userFormData)
 
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    // // check if form has everything (as per react-bootstrap docs)
+    // const form = event.currentTarget;
+    // if (form.checkValidity() === false) {
+    //   event.preventDefault();
+    //   event.stopPropagation();
+    // }
 
-    try {
-      const {data} = await addUser({
-        varaibles: {...userFormData },
-      });
-      console.log(data)
-      Auth.login(data.addUser.token);
-    } catch (err) {
-      console.error(err);
-    }
+    // try {
+    //   const {data} = await addUser({
+    //     varaibles: {...userFormData },
+    //   });
+    //   console.log(data)
+    //   Auth.login(data.addUser.token);
+    // } catch (err) {
+    //   console.error(err);
+    // }
 
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
+    // setUserFormData({
+    //   username: '',
+    //   email: '',
+    //   password: '',
+    // });
   };
 
   return (
