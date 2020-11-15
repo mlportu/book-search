0125360f34bbb2bb82aa3dcd1d249c2cb149f0fc
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-14 18:44:33 -0600

    update signup

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import {useMutation} from '@apollo/react-hooks';
@@ -15,6 +15,14 @@ const SignupForm = () => {
   const [showAlert, setShowAlert] = useState(false);
 
   const [addUser, {error}] = useMutation(ADD_USER);
+
+  useEffect(()=> {
+    if(error) {
+      setShowAlert(true);
+    } else{
+      setShowAlert(false);
+    }
+  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -39,7 +47,6 @@ const SignupForm = () => {
       Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
-      setShowAlert(true);
     }
 
     setUserFormData({
